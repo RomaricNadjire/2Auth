5891785ac861eb5e59c4efce5ee6dce865fb133c
romaric <romaricnadjire07@gmail.com>
2024-06-25 12:21:48 +0000

    Dernier modif

--- src/script.js
+++ src/script.js
@@ -2,36 +2,47 @@ const inputs = document.querySelectorAll('input[type="number"]');
 const submit = document.querySelector('input[type="submit"');
 
 inputs.forEach((input, index) => {
-    input.addEventListener('keyup', (e)=>{
+    input.addEventListener('keyup', (e) => {
         const currentInput = input,
-        nextinput = input.nextElementSibling,
-        prevInput = input.previousElementSibling;
+            nextinput = input.nextElementSibling,
+            prevInput = input.previousElementSibling;
 
-        if(currentInput.value.length>1){
+        if (currentInput.value.length > 1) {
             currentInput.value = input.value.slice(-1);
-            if(index < inputs.length-1){
+            if (index < inputs.length - 1) {
                 nextinput.focus();
-            }else{
-                inputs[0].focus()
-            }            
+            } 
+            // else {
+            //     inputs[0].focus()
+            // }
             return;
         }
 
-        if(nextinput && nextinput.hasAttribute("disabled") && currentInput.value !== ""){
+        if (nextinput && nextinput.hasAttribute("disabled") && currentInput.value !== "") {
             nextinput.removeAttribute("disabled");
             nextinput.focus();
         }
 
-        if(e.key === "Backspace"){
+        if (e.key === "Backspace") {
             inputs.forEach((input, index2) => {
-                if(index <= index2 && prevInput){
+                if (index <= index2 && prevInput) {
                     input.setAttribute('disabled', true);
                     currentInput.value = "";
                     prevInput.focus();
                 }
             });
         }
+
+        let tousNonNuls = true;
+
+        inputs.forEach(input => {
+            if (input.value === '') {
+                tousNonNuls = false;
+            }
+        });
+
+        tousNonNuls?submit.classList.add('active'):submit.classList.remove('active');
     });
 });
 
-window.addEventListener('load', ()=>inputs[0].focus())
+window.addEventListener('load', () => inputs[0].focus())
